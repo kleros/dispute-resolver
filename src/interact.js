9c166a902928499c60e9e13ca3274f9491f1f7c6
Ferit Tunçer <ferit@cryptolab.net>
2019-09-11 15:52:53 +0300

    temp

---
+++ src/interact.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+import Card from 'react-bootstrap/Card'
+import Modal from 'react-bootstrap/Modal'
+import ReactMarkdown from 'react-markdown'
+
+class Interact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { disputeID: '', period: '0' }
+  }
+
+  PERIODS = ['evidence', 'commit', 'vote', 'appeal', 'execution']
+
+  onControlChange = e => this.setState({ [e.target.id]: e.target.value })
+
+  onDisputeIDChange = e => {
+    this.setState({ disputeID: e.target.value })
+  }
+
+  getHumanReadablePeriod = period => this.PERIODS[period]
+
+  render() {
+    const { disputeID, period } = this.state
+
+    return (
+      <Container>
+        {' '}
+        <Card>
+          <Card.Body>
+            <Card.Title>Interact with a Dispute</Card.Title>{' '}
+            <Form>
+              <Form.Row>
+                <Col>
+                  <Form.Group>
+                    <Form.Control
+                      id="disputeID"
+                      as="input"
+                      type="number"
+                      value={disputeID}
+                      onChange={this.onDisputeIDChange}
+                      placeholder={'Dispute identifier'}
+                    />
+                  </Form.Group>
+                </Col>
+                {disputeID && (
+                  <Col>
+                    <Form.Group>
+                      <Form.Control
+                        readOnly
+                        type="text"
+                        placeholder={this.getHumanReadablePeriod(period)}
+                      />
+                    </Form.Group>
+                  </Col>
+                )}
+                {disputeID && (
+                  <Col>
+                    <Form.Group>
+                      <a href={`https://court.kleros.io/cases/${disputeID}`}>
+                        Check out this dispute on Kleros
+                      </a>
+                    </Form.Group>
+                  </Col>
+                )}{' '}
+              </Form.Row>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
+    )
+  }
+}
+
+export default Interact
