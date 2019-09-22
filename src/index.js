ef441d326bd5e4246698d74b216ed7c344160b5f
Ferit Tunçer <ferit@cryptolab.net>
2019-09-23 00:36:54 +0300

    refactor(index): log commit ref in color

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import './index.css'
 import App from './app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+console.log(
+  `%c${process.env.COMMIT_REF}`,
+  `color: #${(process.env.COMMIT_REF || '000000').substr(0, 6)}`
+)
