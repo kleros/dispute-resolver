327201df3b47360f34effc9c11ab68cfd711e682
Ferit Tunçer <ferit@cryptolab.net>
2019-09-27 01:44:26 +0300

    refactor(footer): complete

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,10 +18,42 @@ class Footer extends React.Component {
             <Nav.Link href="/">Binary Arbitrable Proxy</Nav.Link>
           </Nav>{' '}
           <Nav className="abs-end-x ">
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <Nav.Item>
+              {' '}
+              <Nav.Link href="https://t.me/kleros">
+                <div>
+                  I need help <img src="help.svg" alt="help" />
+                </div>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={2} href="https://twitter.com/kleros_io">
+                <img src="twitter.svg" alt="help" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={3} href="https://github.com/kleros">
+                <img src="github.svg" alt="help" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={4} href="https://blog.kleros.io/">
+                <img src="blog.svg" alt="help" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey={5}
+                href="https://www.linkedin.com/company/kleros/"
+              >
+                <img src="linkedin.svg" alt="help" />
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey={6} href="https://t.me/kleros">
+                <img src="telegram.svg" alt="help" />
+              </Nav.Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
