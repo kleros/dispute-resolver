b7609d88bccdd37c1a1fe3a3ab659f91bc61a9bf
brainsale <1965270+brainsale@users.noreply.github.com>
2022-06-02 10:29:34 +0200

    fix race condition and use requestAccounts
    Fix behavior where the web3 account request would wait on a DOM load event that was already fired
    
    Use eth_requestAccounts to allow opening web3 wallet even if locked

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -2,19 +2,29 @@ import Web3 from "web3";
 
 let web3;
 
-window.addEventListener("load", async () => {
-  // Modern dapp browsers...
+const loadWeb3 = async () => {
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     try {
       // Request account access if needed
-      await window.ethereum.request({ method: "eth_accounts" });
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      web3 = new Web3(window.ethereum);
       // Acccounts now exposed
     } catch (_) {
       // User denied account access...
     }
   }
-});
+}
+
+if (document.readyState === "complete") { 
+  // DOM loading is already complete
+  async () => await loadWeb3();
+} else {
+  window.addEventListener("load", async () => {
+    // Modern dapp browsers...
+    await loadWeb3();
+  });
+}
 
 if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   web3 = new Web3(window.ethereum);
