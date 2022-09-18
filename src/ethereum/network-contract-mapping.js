0bbb4029eb60ea91b1adeec6a9da3136680a71dd
Ferit Tunçer <ferit@cryptolab.net>
2022-09-18 05:02:57 -0500

    refactor(map): add currency abbreviations

--- src/ethereum/network-contract-mapping.js
+++ src/ethereum/network-contract-mapping.js
@@ -7,6 +7,7 @@ const map = {
     ARBITRABLE_PROXY: ArbitrableProxy.networks[1]?.address,
     POLICY_REGISTRY: "0xCf1f07713d5193FaE5c1653C9f61953D048BECe4",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_PROVIDER_URL,
+    CURRENCY_SHORT: "ETH"
   },
   3: {
     NAME: "Ethereum Testnet Ropsten",
@@ -14,6 +15,8 @@ const map = {
     ARBITRABLE_PROXY: ArbitrableProxy.networks[3].address,
     POLICY_REGISTRY: "0xe4ee06e5c9921d7e4d4ab62199c8c4b5267a7547",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_ROPSTEN_PROVIDER_URL,
+    CURRENCY_SHORT: "ETH"
+
   },
   4: {
     NAME: "Ethereum Testnet Rinkeby",
@@ -21,6 +24,7 @@ const map = {
     ARBITRABLE_PROXY: "0xe83BCc07C57d2260102C189f780AF6a7bc34a83d",
     POLICY_REGISTRY: "0xCd444af85127392cB84b8583a82e6aE6230Ec0b9",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_RINKEBY_PROVIDER_URL,
+    CURRENCY_SHORT: "ETH"
   },
 
   5: {
@@ -29,6 +33,7 @@ const map = {
     ARBITRABLE_PROXY: "0x78ac5F189FC6DAB261437a7B95D11cAcf0234FFe",
     POLICY_REGISTRY: "0x28c8A3A2E3c8Cd3F795DB83764316a1129a069bA",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_GOERLI_PROVIDER_URL,
+    CURRENCY_SHORT: "ETH"
   },
 
   42: {
@@ -37,6 +42,7 @@ const map = {
     ARBITRABLE_PROXY: ArbitrableProxy.networks[42].address,
     POLICY_REGISTRY: "0xFC53D1d6dDc2C6Cdd403cb7DBf0f26140D82e12d",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_KOVAN_PROVIDER_URL,
+    CURRENCY_SHORT: "ETH"
   },
 
   77: {
@@ -45,6 +51,8 @@ const map = {
     ARBITRABLE_PROXY: ArbitrableProxy.networks[77].address,
     POLICY_REGISTRY: "0x0Bee63bC7220d0Bacd8A3c9d6B6511126CDfe58f",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_SOKOL_PROVIDER_URL,
+    CURRENCY_SHORT: "SPOA"
+
   },
 
   100: {
@@ -53,16 +61,19 @@ const map = {
     ARBITRABLE_PROXY: ArbitrableProxy.networks[100].address,
     POLICY_REGISTRY: "0x9d494768936b6bDaabc46733b8D53A937A6c6D7e",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_XDAI_PROVIDER_URL,
-  },
-
-  80001: {
-    NAME: "Polygon Testnet Mumbai",
-    WEB3_PROVIDER: process.env.REACT_APP_WEB3_MUMBAI_PROVIDER_URL,
+    CURRENCY_SHORT: "xDai"
   },
 
   137: {
     NAME: "Polygon Mainnet",
     WEB3_PROVIDER: process.env.REACT_APP_WEB3_POLYGON_PROVIDER_URL,
+    CURRENCY_SHORT: "MATIC"
+  },
+
+  80001: {
+    NAME: "Polygon Testnet Mumbai",
+    WEB3_PROVIDER: process.env.REACT_APP_WEB3_MUMBAI_PROVIDER_URL,
+    CURRENCY_SHORT: "MATIC"
   },
 
 };
