b15c0ccec1e002a2483cf0266fbf6dc21b94fd29
Ferit Tunçer <ferit@cryptolab.net>
2023-05-02 15:26:29 +0100

    refactor: delete unused variables

--- src/components/unsupportedNetwork.jsx
+++ src/components/unsupportedNetwork.jsx
@@ -1,19 +1,9 @@
-import {  Col } from "react-bootstrap";
 import React from "react";
-import { ReactComponent as Etherscan } from "../assets/images/etherscan.svg";
-
-import networkMap from "../ethereum/network-contract-mapping";
-
-
-import styles from "./styles/footer.module.css";
-
 class UnsupportedNetwork extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    const {  network } = this.props;
-
     return (
       <section>
         UNSUPPORTED NETWORK
@@ -21,5 +11,4 @@ class UnsupportedNetwork extends React.Component {
     );
   }
 }
-
 export default UnsupportedNetwork;
