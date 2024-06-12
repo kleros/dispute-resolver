bbbd52500cfe2efc5c8748e47f155347aa0e0ce6
Ferit <ferit@cryptolab.net>
2024-06-12 11:58:59 +0100

    refactor(app): upgrade packages and maintain dependencies

--- src/containers/404.js
+++ src/containers/404.js
@@ -2,53 +2,31 @@ import { ReactComponent as Acropolis } from "../assets/images/acropolis.svg";
 import PropTypes from "prop-types";
 import React from "react";
 import { Spinner } from "react-bootstrap";
-import styled from "styled-components/macro";
+import styles from "./styles/404.module.css";
 
-const StyledDiv = styled.div`
-  min-height: 100%;
-`;
-const StyledAcropolis = styled(Acropolis)`
-  width: 100%;
-  height: auto;
-`;
-const StyledInfoDiv = styled.div`
-  text-align: center;
-
-  @media (max-width: 500px) {
-    margin: 80px 20px;
-  }
-`;
-const Styled404Div = styled.div`
-  font-size: 40px;
-  font-weight: bold;
-  margin-top: 40px;
-
-  @media (max-width: 500px) {
-    font-size: 30px;
-    line-height: 40px;
-  }
-`;
-const StyledMessageLine1 = styled.div`
-  font-size: 28px;
-  font-weight: bold;
-`;
-const StyledMessageLine2 = styled.div`
-  font-size: 24px;
-  margin-top: 30px;
-`;
-const StyledMessageLine3 = styled.div`
-  font-size: 16px;
-  margin-top: 25px;
-`;
 const _404 = ({ Web3 }) => (
-  <StyledDiv Web3={Web3}>
-    <StyledAcropolis />
-    <StyledInfoDiv>
-      <Styled404Div className="primary-color theme-color">{Web3 ? "Connecting Ethereum..." : "404"}</Styled404Div>
-      <StyledMessageLine2 className="ternary-color theme-color">{Web3 ? "The gods are trying to locate your Web3 provider." : "Something went wrong in Athens!"}</StyledMessageLine2>
-      {Web3 && <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" style={{ width: "5rem", height: "5rem" }} className="purple-inverted" />}
-    </StyledInfoDiv>
-  </StyledDiv>
+  <div className={styles.styledDiv} Web3={Web3}>
+    <Acropolis className={styles.styledAcropolis} />
+    <div className={styles.styledInfoDiv}>
+      <div className={`${styles.styled404Div} primary-color theme-color`}>
+        {Web3 ? "Connecting Ethereum..." : "404"}
+      </div>
+      <div className={`${styles.styledMessageLine2} ternary-color theme-color`}>
+        {Web3 ? "The gods are trying to locate your Web3 provider." : "Something went wrong in Athens!"}
+      </div>
+      {Web3 && (
+        <Spinner
+          as="span"
+          animation="grow"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+          style={{ width: "5rem", height: "5rem" }}
+          className="purple-inverted"
+        />
+      )}
+    </div>
+  </div>
 );
 
 _404.propTypes = {
